b427bcac89d6988b3dafa4555665c27776ad04fc
ramit110 <Ramit110@users.noreply.github.com>
2025-01-25 17:56:35 +0000

    removed important code lmao

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -60,6 +60,10 @@ where
         let params: Path<HashMap<String, String>> =
             parts.extract().await.map_err(IntoResponse::into_response)?;
 
+        let version = params
+            .get("version")
+            .ok_or_else(|| (StatusCode::NOT_FOUND, "version param missing").into_response())?;
+
         match version.as_str() {
             "v1" => Ok(Version::V1),
             _ => Err((StatusCode::NOT_FOUND, "unknown version").into_response()),
